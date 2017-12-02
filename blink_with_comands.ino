0d4b1b52261e5cde02421782631a534126683d14
egonzalez <everardo.gonzalez@students.olin.edu>
2017-12-02 17:10:19 -0500

    adding blink with commands

---
+++ blink_with_comands.ino
@@ -0,0 +1,49 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+// This adds in a state
+int state;
+int led = 13;
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(led, OUTPUT);
+  Serial.begin(9600); // this initializes receiving commands from serial
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  if (Serial.available() > 0) { // more than zero means that there is information being transmitted
+    if (Serial.peek() == 'c') {
+      Serial.read();
+      state = Serial.parseInt();
+      digitalWrite(led,state);
+    }
+    while (Serial.available() > 0) {
+      Serial.read();
+    }
+  }
+}
+
